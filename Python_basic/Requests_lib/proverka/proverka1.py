d001769ac3321a5657d8863cee0b615d0305f751
DenisZaychikov <denis.zaj4ikov@yandex.ru>
2021-07-07 16:36:41 +0300

    Delete Python_basic/Requests_lib/proverka directory

--- Python_basic/Requests_lib/proverka/proverka1.py
+++
@@ -1,8 +0,0 @@
-import requests
-from pprint import pprint
-
-data = {'name': 'Petr', 'surname': 'Petrov'}
-url = 'https://httpbin.org/post'
-# response = requests.get(url, params=data)
-response = requests.post(url, data=data)
-pprint(response.json())
